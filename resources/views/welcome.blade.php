da5a78d3e876b0d47230749063cdd7eeb1bc9093
Stanislav <shiptya@icloud.com>
2018-04-02 21:18:17 +0300

    add voyager files and do template layoult

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,245 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Academy childhood</title>
+    <link rel="SHORTCUT ICON" href="images/logo_tab.png" type="image/jpg">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+</head>
+<body>
 
-        <title>Laravel</title>
+<header class="header-wrapper">
+    <div class="header-top">
+        <div class="container">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="container">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        </div>
+    </div>
+</header>
 
-            .full-height {
-                height: 100vh;
-            }
+<section class="big-slider">
+    <div class="big-slider-body">
+        <div><img src="images/header_slide.png"></div>
+        <div><img src="images/header_slide.png"></div>
+        <div><img src="images/header_slide.png"></div>
+        <div><img src="images/header_slide.png"></div>
+        <div><img src="images/header_slide.png"></div>
+        <div><img src="images/header_slide.png"></div>
+    </div>
+</section>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<section class="content">
 
-            .position-ref {
-                position: relative;
-            }
+    <div class="wrapper-main-news-services">
+        <div class="container">
+            <div class="row">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+                <div class="col-md-8">
+                    <h3></h3>
 
-            .content {
-                text-align: center;
-            }
+                    <div class="wrapper-main-news">
 
-            .title {
-                font-size: 84px;
-            }
+                    </div>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="col-md-4">
+                    <h3></h3>
+
+                    <div class="wrapper-sidebar-services">
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper-news-anonses">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h3></h3>
+
+                    <div class="wrapper-news">
+                        <div class="row">
+                            <div class="wrapper-news-item">
+                                <div class="row">
+                                    <div class="col-md-4"></div>
+                                    <div class="col-md-8"></div>
+                                </div>
+
+                                <div class="wrapper-news-item-bottom">
+                                    <div class="row">
+                                        <div class="col-md-6"></div>
+                                        <div class="col-md-6"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="wrapper-news-item">
+                                <div class="row">
+                                    <div class="col-md-4"></div>
+                                    <div class="col-md-8"></div>
+                                </div>
+
+                                <div class="wrapper-news-item-bottom">
+                                    <div class="row">
+                                        <div class="col-md-6"></div>
+                                        <div class="col-md-6"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="wrapper-news-item">
+                                <div class="row">
+                                    <div class="col-md-4"></div>
+                                    <div class="col-md-8"></div>
+                                </div>
+
+                                <div class="wrapper-news-item-bottom">
+                                    <div class="row">
+                                        <div class="col-md-6"></div>
+                                        <div class="col-md-6"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="wrapper-news-pagination">
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="wrapper-anonses-heading">
+
+                    </div>
+                    <div class="wrapper-anonses-list">
+
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</section>
+
+<section class="gallery">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-6"></div>
+                    <div class="col-md-6"></div>
+                    <div class="col-md-6"></div>
+                    <div class="col-md-6"></div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="row">
+                    <div class="col-md-12"></div>
+                    <div class="col-md-12"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="wrapper-gallery-bottom">
+            <div class="row">
+                <div class="col-md-8"></div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="links">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+                <div class="wrapper-link-item">
+                    <img src="" alt="">
+                </div>
+            </div>
+
+            <div class="col-md-2">
+                <div class="wrapper-link-item">
+                    <img src="" alt="">
+                </div>
+            </div>
+
+            <div class="col-md-2">
+                <div class="wrapper-link-item">
+                    <img src="" alt="">
+                </div>
+            </div>
+
+            <div class="col-md-2">
+                <div class="wrapper-link-item">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer class="footer">
+
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-12">
+                    <div class="wrapper-footer-logo">
+                        <img src="" alt="footer-logo">
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    <div class="wrapper-footer-social">
+
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    <div class="wrapper-footer-additional">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <p class="copyright"></p>
+        </div>
+    </div>
+</footer>
+
+<script src="js/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+        integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+        integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"
+        integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
+        crossorigin="anonymous"></script>
+<script src="js/fontawesome-all.min.js"></script>
+</body>
 </html>
