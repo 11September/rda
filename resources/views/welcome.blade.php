033ababc94ccbe8c8feef96917450f2144761c3c
Stanislav <shiptya@icloud.com>
2018-04-03 11:29:15 +0300

    ** Footer markup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -201,23 +201,38 @@
 </section>
 
 <footer class="footer">
-
     <div class="footer-top">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-12">
+            <div class="row wrapper-footer-top-content">
+                <div class="col-lg-3 col-md-12 center-block">
                     <div class="wrapper-footer-logo">
                         <img src="" alt="footer-logo">
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-12">
-                    <div class="wrapper-footer-social">
-
+                <div class="col-lg-3 col-md-12 center-block">
+                    <div class="wrapper-footer-languages">
+                        <ul class="footer-languages-menu list-inline">
+                            <li class="list-inline-item footer-languages-menu-item"><a href="#">УКР</a></li>
+                            <li class="list-inline-item footer-languages-menu-item"><a href="#">РУС</a></li>
+                            <li class="list-inline-item footer-languages-menu-item"><a href="#">ENG</a></li>
+                        </ul>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-12">
+                <div class="col-lg-3 col-md-12 center-block">
+                    <div class="wrapper-footer-social">
+                        <a class="footer-social-item" href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a class="footer-social-item" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="footer-social-item" href="#"><i class="fab fa-youtube"></i></a>
+                        <a class="footer-social-item" href="#"><i class="fas fa-rss"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-12 center-block">
                     <div class="wrapper-footer-additional">
-
+                        <ul class="footer-additional-menu">
+                            <li class="footer-additional-menu-item"><a href="#"><i class="fas fa-users"></i>Звернення громадян</a></li>
+                            <li class="footer-additional-menu-item"><a href="#"><i class="fas fa-sitemap"></i>Мапа сайту</a></li>
+                            <li class="footer-additional-menu-item"><a href="#"><i class="fas fa-envelope"></i>Контактна інформація</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -225,7 +240,11 @@
     </div>
     <div class="footer-bottom">
         <div class="container">
-            <p class="copyright"></p>
+            <div class="row wrapper-footer-bottom">
+                <div class="col-md-12">
+                    <p class="copyright">Разработано <a href="#"><span class="red">LocalService</span></a></p>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
