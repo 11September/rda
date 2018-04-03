cfc3003d2865eaeebfca5e5d7024b390d17c162d
Stanislav <shiptya@icloud.com>
2018-04-03 15:25:19 +0300

    ** Page news without responsive with different sections

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,17 +1,38 @@
 @extends('layouts.master')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/service.css') }}">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.css">
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.css') }}">
 @endsection
 
 @section('content')
-
+    <section class="big-slider">
+        <div class="big-slider-body owl-carousel  owl-theme">
+            <div><img src="{{ asset('images/slide.png') }}"></div>
+            <div><img src="{{ asset('images/slide.png') }}"></div>
+            <div><img src="{{ asset('images/slide.png') }}"></div>
+            <div><img src="{{ asset('images/slide.png') }}"></div>
+            <div><img src="{{ asset('images/slide.png') }}"></div>
+            <div><img src="{{ asset('images/slide.png') }}"></div>
+        </div>
+    </section>
 
     <section class="news-page">
+
+        <div class="wrapper-news-heading">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>Новини</h2>
+                    </div>
+                    <div class="col-md-6">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="container">
-            <h2>Новини</h2>
             <div class="row">
                 <div class="col-xl-12 news_left_block">
 
@@ -364,33 +385,162 @@
                             </div>
                         </div>
                     </div>
-
-
                 </div>
-
-
-                <nav class="news_navigation" aria-label="Page navigation">
-                    <ul class="pagination">
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link page-link-active" href="#">3</a></li>
-                        <li class="page-item"><a class="page-link" href="#">4</a></li>
-                        <li class="page-item"><a class="page-link" href="#">5</a></li>
-                        <li class="page-item"><a class="page-link" href="#">6</a></li>
-                        <li class="page-item"><a class="page-link" href="#">7</a></li>
-                        <li class="page-item"><a class="page-link" href="#">8</a></li>
-                        <li class="page-item"><a class="page-link" href="#">9</a></li>
-                        <li class="page-item add_info"><a class="page-link" href="#">>></a></li>
-                        <li class="page-item add_info add_info_news"><a class="page-link" href="#">Всі новини</a>
-                        </li>
-                    </ul>
-                </nav>
             </div>
         </div>
+
+        <div class="wrapper-pagination">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination justify-content-center">
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link page-link-active" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">4</a></li>
+                                <li class="page-item"><a class="page-link" href="#">5</a></li>
+                                <li class="page-item"><a class="page-link" href="#">6</a></li>
+                                <li class="page-item"><a class="page-link" href="#">7</a></li>
+                                <li class="page-item"><a class="page-link" href="#">8</a></li>
+                                <li class="page-item"><a class="page-link" href="#">9</a></li>
+                                <li class="page-item add_info"><a class="page-link" href="#">>></a></li>
+                                <li class="page-item add_info add_info_news"><a class="page-link" href="#">Всі
+                                        новини</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="links">
+        <div class="container">
+
+            <h2>ПОСИЛАННЯ</h2>
+
+            <div class="wrapper-links-content">
+                <div class="row">
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_1.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_2.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_3.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_4.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_5.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_6.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_7.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_8.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_9.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_10.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_11.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_12.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3">
+                        <div class="wrapper-link-item">
+                            <a href="#">
+                                <img src="{{ asset('images/link_item_13.png') }}" alt="link">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
 
 @section('scripts')
-
+    <script src="js/owl.carousel.min.js"></script>
+    <script>
+        $('.big-slider-body').owlCarousel({
+            loop: true,
+            margin: 10,
+            items: 1
+        });
+    </script>
 @endsection
