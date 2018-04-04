007916d2f809aa68421c0422ae4af5481ccb41f0
nosslsvec8 <nosiljevets@gmail.com>
2018-04-04 12:10:34 +0300

    fix

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -508,64 +508,6 @@
                             <div class="post-item">
                                 <div class="row">
                                     <div class="col-xl-6 news_item_img">
-                                        <img src="{{ asset('images/news/news_item_8.png') }}" alt="news_item_img">
-                                    </div>
-                                    <div class="col-xl-6 post-item-description">
-                                        <div class="row">
-                                            <div class="col-md-12">
-
-                                                <div class="post-item-calender">
-                                                    <i class="fas fa-calendar-alt"></i>
-                                                    <p class="post-item-calender-date">14.03.2018</p>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <h3>Подія дня, чи цієї години. Заголовок набраний висотою 20пт.</h3>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="wrapper-post-content">
-                                            <p class="post-content">
-                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                                veniam,
-                                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                                consequat...
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="clearfix"></div>
-
-                                <div class="post-item-bottom">
-                                    <div class="row">
-                                        <div class="col-md-7">
-                                            <div class="wrapper-post-item-category">
-                                                <i class="fas fa-folder-open"></i>
-                                                <p class="post-item-category">ОСТАННІ НОВИНИ</p>
-
-                                                <i class="fas fa-user"></i>
-                                                <p class="post-item-user">Админ.</p>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-5">
-                                            <a class="post-item-more" href="#">
-                                                <span>Докладніше...</span>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-6">
-                            <div class="post-item">
-                                <div class="row">
-                                    <div class="col-xl-6 news_item_img">
                                         <img src="{{ asset('images/news/news_item_9.png') }}" alt="news_item_img">
                                     </div>
                                     <div class="col-xl-6 post-item-description">
