670d04beae074832ec9f31a8f34ebd74a2b54b4b
nosslsvec8 <nosiljevets@gmail.com>
2018-04-04 13:44:59 +0300

    fix "news"

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -694,14 +694,14 @@
     </section>
 
 
-    <section class="links">
+       <section class="links">
         <div class="container">
 
             <h2>ПОСИЛАННЯ</h2>
 
             <div class="wrapper-links-content">
                 <div class="row">
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_1.png') }}" alt="link">
@@ -709,7 +709,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_2.png') }}" alt="link">
@@ -717,7 +717,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_3.png') }}" alt="link">
@@ -725,7 +725,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_4.png') }}" alt="link">
@@ -733,7 +733,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_5.png') }}" alt="link">
@@ -741,7 +741,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_6.png') }}" alt="link">
@@ -749,7 +749,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_7.png') }}" alt="link">
@@ -757,7 +757,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_8.png') }}" alt="link">
@@ -765,7 +765,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_9.png') }}" alt="link">
@@ -773,7 +773,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_10.png') }}" alt="link">
@@ -781,7 +781,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_11.png') }}" alt="link">
@@ -789,7 +789,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_12.png') }}" alt="link">
@@ -797,7 +797,7 @@
                         </div>
                     </div>
 
-                    <div class="col-md-2 col-sm-3">
+                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-4">
                         <div class="wrapper-link-item">
                             <a href="#">
                                 <img src="{{ asset('images/link_item_13.png') }}" alt="link">
