8d3c2968838c0185c43897d7216fbf4c1f94951b
nosslsvec8 <nosiljevets@gmail.com>
2018-04-04 13:51:00 +0300

    add . tab_logo

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -3,6 +3,7 @@
 @section('css')
     <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
 @endsection
 
 @section('content')
