eb4e3e0cc519ee56399b6162c5fc8115447aa0ab
Stanislav <shiptya@icloud.com>
2018-04-04 15:07:09 +0300

    ** Fixes title all page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
     <![endif]-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>RDA</title>
+    <title>Відділ освіти </title>
     {{--<title>{{ Voyager::setting('title') }}</title>--}}
     {{--<meta name="description" content="{{ Voyager::setting('description') }}">--}}
     {{--<meta name="keywords" content="{{ Voyager::setting('keywords') }}">--}}
