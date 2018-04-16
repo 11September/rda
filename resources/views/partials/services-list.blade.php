ccd92eb7604a0c8333d9640dc95f81d8a92b7f1f
Stanislav <shiptya@icloud.com>
2018-04-16 20:00:13 +0300

    ** Services list

--- resources/views/partials/services-list.blade.php
+++ resources/views/partials/services-list.blade.php
@@ -2,7 +2,6 @@
 
     <div class="wrapper-services-list-item">
         <div class="services-list-item">
-
             <div class="services-list-item-picture">
                 <img src="{{ asset('images/services_item_1.png') }}" alt="services">
             </div>
@@ -11,7 +10,6 @@
                     <p class="services-list-item-text">Адміністративні послуги</p>
                 </a>
             </div>
-
         </div>
     </div>
 
@@ -29,13 +27,129 @@
     </div>
 
     <div class="wrapper-services-list-item">
-        <div class="services-list-item services-last-item">
+        <div class="services-list-item ">
             <div class="services-list-item-picture">
                 <img src="{{ asset('images/services_item_3.png') }}" alt="services">
             </div>
             <div class="services-list-item-content">
                 <a href="#">
                     <p class="services-list-item-text">Система електронних зверненнь</p>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper-services-list-item">
+        <div class="services-list-item">
+            <div class="services-list-item-picture">
+                <img src="{{ asset('images/services_item_1.png') }}" alt="services">
+            </div>
+            <div class="services-list-item-content">
+                <a href="#">
+                    <p class="services-list-item-text">
+                        <a data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                            Нормативно-правова база
+                        </a>
+                    </p>
+                </a>
+            </div>
+        </div>
+
+        <div class="collapse" id="collapseExample">
+            <div class="card-block">
+                 <div class="wrapper-collapse-services">
+                     <p>
+                         <a href="#" class="collapse-one-service">Закони</a>
+                     </p>
+                     <p>
+                         <a href="#" class="collapse-one-service">Укази Президента</a>
+                     </p>
+                     <p>
+                         <a href="#" class="collapse-one-service">Постанови КМУ</a>
+                     </p>
+                     <p>
+                         <a href="#" class="collapse-one-service">Накази </a>
+                     </p>
+                 </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper-services-list-item">
+        <div class="services-list-item">
+            <div class="services-list-item-picture">
+                <img src="{{ asset('images/services_item_1.png') }}" alt="services">
+            </div>
+            <div class="services-list-item-content">
+                <a href="#">
+                    <p class="services-list-item-text">
+                        <a data-toggle="collapse" href="#collapseExample1" aria-expanded="false" aria-controls="collapseExample1">
+                            Доступ до публiчноi iнформацii
+                        </a>
+                    </p>
+                </a>
+            </div>
+        </div>
+
+        <div class="collapse" id="collapseExample1">
+            <div class="card-block">
+                <div class="wrapper-collapse-services">
+                    <p>
+                        <a href="#" class="collapse-one-service">Нормативні акти</a>
+                    </p>
+                    <p>
+                        <a href="#" class="collapse-one-service">Накази відділу освіти з основної діяльності</a>
+                    </p>
+                    <p>
+                        <a href="#" class="collapse-one-service">Програми  </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper-services-list-item">
+        <div class="services-list-item">
+            <div class="services-list-item-picture">
+                <img src="{{ asset('images/services_item_1.png') }}" alt="services">
+            </div>
+            <div class="services-list-item-content">
+                <a href="#">
+                    <p class="services-list-item-text">
+                        <a data-toggle="collapse" href="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2">
+                            Звернення Громадян
+                        </a>
+                    </p>
+                </a>
+            </div>
+        </div>
+
+        <div class="collapse" id="collapseExample2">
+            <div class="card-block">
+                <div class="wrapper-collapse-services">
+                    <p>
+                        <a href="#" class="collapse-one-service">Нормативні акти</a>
+                    </p>
+                    <p>
+                        <a href="#" class="collapse-one-service">Графік прийому</a>
+                    </p>
+                    <p>
+                        <a href="#" class="collapse-one-service">Інформація про роботу зі зверненнями</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="wrapper-services-list-item">
+        <div class="services-list-item services-last-item">
+            <div class="services-list-item-picture">
+                <img src="{{ asset('images/services_item_5.png') }}" alt="services">
+            </div>
+            <div class="services-list-item-content">
+                <a href="#">
+                    <p class="services-list-item-text">Державнi Закупiвлi</p>
                 </a>
             </div>
         </div>
