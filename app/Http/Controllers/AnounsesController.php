9f1088512b0e47433a4537db012dbe59231a78eb
Stanislav <shiptya@icloud.com>
2018-04-17 19:21:31 +0300

    ** many **

---
+++ app/Http/Controllers/AnounsesController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\News;
+use Illuminate\Http\Request;
+
+class AnounsesController extends Controller
+{
+    public function index()
+    {
+        $posts = News::where('status', 'published')->paginate(1);
+
+        return view('news', compact('posts'));
+    }
+
+    public function post($id = null)
+    {
+        $post = News::where('id', $id)->firstOrFail();
+
+        $previous = News::where('id', '<', $post->id)->orderBy('id','desc')->first();
+
+        $next = News::where('id', '>', $post->id)->orderBy('id')->first();
+
+        return view('post', compact('post', 'previous', 'next'));
+    }
+
+    public function search(Request $request)
+    {
+        $request->validate([
+            'search' => 'required|max:255',
+        ]);
+
+        $query = $request->search;
+
+        $posts = News::where('status', 'published')
+            ->where('title', 'LIKE', '%' . $query . '%')
+            ->paginate(1);
+
+        return view('search', compact('posts', 'query'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', 'WelcomeController@index');
 Route::get('/news', 'NewsController@index');
 Route::get('/news/{news}', 'NewsController@post');
 Route::get('/search', 'NewsController@search');
+
+Route::get('/anounses', 'AnounsesController@index');
+Route::get('/anounses/{anounse}', 'AnounsesController@post');
+
 //Route::get('/contact', 'WelcomeController@contacts');
 
 Route::group(['prefix' => 'admin'], function () {
