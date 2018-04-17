36df86d786a6d4a473500ae7f86c1042e274ee85
nosslsvec8 <nosiljevets@gmail.com>
2018-04-17 20:30:39 +0300

    fix albom img

--- resources/views/album.blade.php
+++ resources/views/album.blade.php
@@ -26,7 +26,7 @@
 
                         <div class="row">
                             @foreach($photos as $photo)
-                                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                <div class="col-xl-4 col-lg-6 col-md-12 col-sm-12 col-xs-12 albom_item_img">
                                     <a data-fancybox="photos" href="{{ asset('storage/' . $photo->image) }}">
                                         <img src="{{ asset('storage/' . $photo->image) }}" alt="">
                                     </a>
