b29fa41b8482c86751d646122a33847af1f845aa
Stanislav <shiptya@icloud.com>
2018-04-25 16:00:47 +0300

    ** Add relations in models

--- app/Album.php
+++ app/Album.php
@@ -7,9 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Album extends Model
 {
+    protected $table = 'albums';
+
     public static function thubnails_photos()
     {
         $photos = Album::select('id','image', 'created_at')->latest()->take(6)->get();
         return $photos;
+    }
+
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
     }
 }
--- app/Photo.php
+++ app/Photo.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
-    
+    protected $table = 'photos';
+
+    public function album()
+    {
+        return $this->belongsTo(Album::class, 'album_id');
+    }
 }
